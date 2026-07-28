2f0f48696dfe6cb3b8b2f16619a8a9b2a58ef4c0
pedidoll <gaebalciki@gmail.com>
2026-07-28 20:23:28 +0900

    오늘의 커밋

---
+++ algorithm/sort/insertion sort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (void) {
+    int arr[10] = {1,10,5,8,7,6,4,3,2,9};
+    int i, j, temp;
+    for (i=0; i<9; i++) {
+        j = i;
+        while (arr[j] > arr[j+1]) {
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+            j--;
+        }
+    }
+    for (i=0; i<10; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
